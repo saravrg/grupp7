3655bc9f2ec34491d8fb6be2b90994483b460527
Sara <saravarghaei@hotmail.com>
2021-11-25 18:26:22 +0100

    Commented, added hamburger menu, cleaned up

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// This is an array with the anchor-tags from the nav-bar
 var nav = ["News", "About us", "Press", "Careers", "Contact"];
 console.log( nav[0] );
 console.log( nav[1] );
@@ -6,6 +7,7 @@ console.log( nav[3] );
 console.log( nav[4] );
 console.log(nav.length);
 
+// This is a for-loop used to repeat a specific block of code (the navbar)
 var arrayLength = nav.length;
 var myList = "<ul>";
 for (var i = 0; i < arrayLength; i++) { myList += "<li>" + nav[i] + "</li>"; } myList += "</ul>";
