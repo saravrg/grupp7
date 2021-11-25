b77da22b8ccbcbb06d1f9fb6f843d803621c3ad1
P Arnell <epost@philiparnell.com>
2021-11-25 14:50:08 +0100

    Added array and loop in Javascript

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+var nav = ["News", "About us", "Press", "Careers", "Contact"];
+console.log( nav[0] );
+console.log( nav[1] );
+console.log( nav[2] );
+console.log( nav[3] );
+console.log( nav[4] );
+console.log(nav.length);
+
+var arrayLength = nav.length;
+var myList = "<ul>";
+for (var i = 0; i < arrayLength; i++) { myList += "<li>" + nav[i] + "</li>"; } myList += "</ul>";
+document.getElementById("myTopnav").innerHTML = myList;
